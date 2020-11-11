974de4f12e50e4d7921975d7489b2f9f62e93a50
Sadupa Wijeratne <sadupag@hsenidmobile.com>
2020-11-11 16:52:15 +0530

    Integrated liquibase

---
+++ src/main/resources/db/changelog/changes/initial-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO shop (name, address, telephone) VALUES ('Union Place','147, Union Place, Colombo','0112475862');
+INSERT INTO customer (name, address, telephone) VALUES ('John','187, Kotta Road, Rajagiriya','0777485321');
+INSERT INTO item (name, price) VALUES ('Cappuccino',200.00);
+INSERT INTO item (name, price) VALUES ('Espresso',220.00);
